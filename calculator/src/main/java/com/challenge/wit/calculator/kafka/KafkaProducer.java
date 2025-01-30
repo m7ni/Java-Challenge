3f2fd46c135e423d7e5b04b640c806abcf92abba
m7ni <a2020142829@isec.pt>
2025-01-30 23:05:42 +0000

    docker build

--- calculator/src/main/java/com/challenge/wit/calculator/kafka/KafkaProducer.java
+++ calculator/src/main/java/com/challenge/wit/calculator/kafka/KafkaProducer.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaException;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
